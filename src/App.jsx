28ae50786479a5cbcc575cfa11596a9a244c4809
YogaAlfaRisqi <163861567+YogaAlfaRisqi@users.noreply.github.com>
2025-07-10 12:11:04 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div class="flex items-center justify-center h-screen">
+        <h1 class="text-3xl font-semibold text-center text-blue-400">Starter pack</h1>
+      </div>
+    </>
+  );
+}
